f472739ffc9de7b8112430ee39b598b0bb182ff6
AbdullahAldaghaym <Abdullah97sd@gmail.com>
2025-12-28 21:27:01 +0000

    Task 1: Add Betty-compliant skeleton

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "simple_shell.h"
+
+/**
+ * main - entry point for the simple shell.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
+	return (0);
+}
+
---
+++ simple_shell.h
@@ -0,0 +1,13 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+/*
+ * File: simple_shell.h
+ *
+ * Desc: Header file for the simple shell project.
+ */
+
+int main(int argc, char **argv);
+
+#endif /* SIMPLE_SHELL_H */
+
