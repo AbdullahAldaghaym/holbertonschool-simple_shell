a99fa12cc976d37138de7771b6fc177427f11145
i1SaSa <s9ny398335@gmail.com>
2025-12-29 17:44:59 +0300

    Add missing header includes in simple_shell.h

--- simple_shell.h
+++ simple_shell.h
@@ -1,6 +1,12 @@
 #ifndef SIMPLE_SHELL_H
 #define SIMPLE_SHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
 /*
  * File: simple_shell.h
  *
@@ -10,4 +16,3 @@
 int main(int argc, char **argv);
 
 #endif /* SIMPLE_SHELL_H */
-
