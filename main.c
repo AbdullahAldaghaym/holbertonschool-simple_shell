0f30f280966332c470913f3bb95bcc712ef53897
i1SaSa <s9ny398335@gmail.com>
2025-12-29 16:17:06 +0300

    Implement fork and execve for command execution

--- main.c
+++ main.c
@@ -5,11 +5,17 @@
  *
  * Return: Always 0
  */
+
+extern char **environ;
+
 int main(void)
 {
 	char *buffer = NULL;
 	size_t n = 0;
 	ssize_t read;
+	char *argv[2]; 
+	pid_t child_pid;
+	int status;
 
 	while (1)
 	{
@@ -17,7 +23,6 @@ int main(void)
 			write(STDOUT_FILENO, "$ ", 2);
 
 		read = getline(&buffer, &n, stdin);
-
 		if (read == -1)
 		{
 			free(buffer);
@@ -26,8 +31,31 @@ int main(void)
 
 		if (buffer[read - 1] == '\n')
 			buffer[read - 1] = '\0';
-	}
 
+		argv[0] = buffer;
+		argv[1] = NULL;
+
+		child_pid = fork();
+
+		if (child_pid == -1)
+		{
+			perror("Error:");
+			return (1);
+		}
+
+		if (child_pid == 0)
+		{
+			if (execve(argv[0], argv, environ) == -1)
+			{
+				perror("./shell"); 
+			}
+			exit(0);
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
 	free(buffer);
 	return (0);
 }
