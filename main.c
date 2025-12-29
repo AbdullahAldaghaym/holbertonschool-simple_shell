ac1254a6ad5927e5d0f40d0489b225b6af97915e
i1SaSa <s9ny398335@gmail.com>
2025-12-29 17:44:45 +0300

    Refactor main function for clarity and consistency

--- main.c
+++ main.c
@@ -1,17 +1,62 @@
 #include "simple_shell.h"
 
 /**
- * main - entry point for the simple shell.
- * @argc: argument count.
- * @argv: argument vector.
+ * main - Simple Shell main function
  *
- * Return: Always 0.
+ * Return: Always 0
  */
-int main(int argc, char **argv)
-{
-	(void)argc;
-	(void)argv;
 
+extern char **environ;
+
+int main(int ac, char **av)
+{
+	char *buffer = NULL;
+	size_t n = 0;
+	ssize_t read;
+	char *args[2];
+	pid_t child_pid;
+	int status;
+
+	(void)ac;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "$ ", 2);
+
+		read = getline(&buffer, &n, stdin);
+		if (read == -1)
+		{
+			free(buffer);
+			exit(0);
+		}
+
+		if (buffer[read - 1] == '\n')
+			buffer[read - 1] = '\0';
+
+		args[0] = buffer;
+		args[1] = NULL;
+
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("Error");
+			return (1);
+		}
+		if (child_pid == 0)
+		{
+			if (execve(args[0], args, environ) == -1)
+			{
+
+				perror(av[0]);
+			}
+			exit(0);
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
+	free(buffer);
 	return (0);
 }
-
